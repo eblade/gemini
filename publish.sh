309e66fe8a2aa757ecf1c3d47f5b61413f9db7e2
Johan Egneblad <johan@egneblad.se>
2022-02-12 21:31:56 +0100

    There should be a script

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PUBLISH_DIR="$1"
+
+[ -z "$PUBLISH_DIR" ] && echo "Missing argument PUBLISH_DIR" && exit 1
+
+PUBLISH_DIR=$(readlink -f "$PUBLISH_DIR")
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $dir
+
+[ ! -d "$PUBLISH_DIR" ] && echo "PUBLISH_DIR does not exist" && exit 2
+
+rm -r $PUBLISH_DIR/*
+cp -r content/* "$PUBLISH_DIR"
